b085f74e1c1d99bbbe6c2f7d5a1aadcadb9fb777
github username <github email address>
2019-03-15 15:04:03 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind
+    if !(@s1 > 0 && @s2 > 0 && @s3 > 0 && (@s1 + @s2 > @s3) && (@s1 + @s3 > @s2) && (@s2 + @s3 > @s1))
+      raise TriangleError
+    else
+      if @s1 == @s2 && @s2 == @s3 && @s1 == @s3
+        :equilateral
+      elsif @s1 == @s2 || @s2 == @s3 || @s1 == @s3
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
+
 end
